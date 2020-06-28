6bc7659f9041234b89f6ed7de3720390e74ee249
farewell12345 <1149558764@qq.com>
2020-06-28 11:55:27 +0800

    commnet

---
+++ WifeClass.py
@@ -0,0 +1,190 @@
+import linecache
+import random
+import sched
+import threading
+import time
+
+import requests
+from zhon.hanzi import punctuation
+from awesome.plugins.NewWife.random_config_index import *
+
+
+# 老公类，用于绑定用户账号和计算渣男值
+class husband:
+    def __init__(self, ID: str):
+        self.ID = ID
+        self.FuckingBoy = 0
+
+    def getClass(self):
+        return self
+
+    # 判断这个用户是不是渣男
+    def isFuckingBoy(self) -> bool:
+        return True if self.FuckingBoy > 20 else False
+
+
+def Random(tuple: ()):
+    return random.randint(0, len(tuple) - 1)
+
+
+def WifeHair(HairColor: str, HairShape) -> str:
+    return HairColor + HairShape
+
+
+def getScence() -> str:
+    try:
+        url = "https://chp.shadiao.app/api.php"
+        data = requests.get(url, timeout=3).text
+        with open('love.txt', 'a', encoding='gbk') as f:
+            unicodes = data.encode('gbk').decode('gbk')
+            f.write(str(unicodes + '\n'))
+            f.close()
+    except:
+        data = linecache.getline(r'love.txt', random.randrange(0, len(open('love.txt', 'gbk').readlines())))
+    return data
+
+
+# 老婆生成类
+def is_rightful(word: str):  # 判断是不是阳间文字
+    for ch in word:
+        if not ('\u4e00' <= ch <= '\u9fff' or 'a' <= ch <= 'z' or 'A' <= ch <= 'Z'):
+            return False
+    return True
+
+
+import string
+
+stri = 'today is friday, so happy..!!!'
+
+
+def deletePunctuation(stri: str) -> str:  # 去除标点符号
+    stri = stri.replace(' ', '').replace('\n', '')
+    punctuation_string = string.punctuation
+    for i in punctuation_string:
+        stri = stri.replace(i, '')
+    for i in punctuation:
+        stri = stri.replace(i, '')
+    return stri
+
+
+class WifeObj:
+    def getMarry(self) -> str:
+        if self.liking > 100 and not self.isMerry:
+            self.isMerry = True
+        elif self.isMerry:
+            return '我们已经结婚了哦'
+        return MarriageVow[Random(MarriageVow)] if self.liking > 100 else '你太着急了，让我们再培养培养感情吧'
+
+    def getDict(self) -> dict:
+        return {
+            'name': self.name,
+            'age': self.age,
+            'husband': self.husband.ID,
+            'ouBai': self.ouBai,
+            'height': self.height,
+            'weight': self.weight,
+            'character': self.Character,
+            'bud': self.bud,
+            'isMerry': self.isMerry,
+            'liking': self.liking,
+            'work': self.work,
+            'race': self.race,
+            'WifeNickName': self.WifeNickName,
+            'HusbandNickName': self.HusbandNickName,
+            'hair': self.Hair,
+            'eyesColor': self.eyesColor
+        }
+
+    def setNickName(self, NickName: str, isWife: bool):
+        NickName = deletePunctuation(NickName)
+        if len(NickName) < 1: return '?'
+        if random.randint(0, self.liking) < 30 or not is_rightful(NickName) or len(NickName) >= 10:
+            return '好难念的名字，我不要'
+        if isWife:
+            self.WifeNickName = NickName
+            return f'好'
+        else:
+            self.HusbandNickName = NickName
+            return f'好的{self.HusbandNickName}'
+
+    def __init__(self, thisHusband: husband):
+        self.husband = thisHusband # 绑定husband对象
+        self.WifeNickName = '老婆'  # 你叫女朋友为‘老婆’
+        self.HusbandNickName = "老公"  # 女朋友叫你,默认为老公
+
+        self.eyesColor = Color[Random(Color)]  # 瞳色
+
+        self.work = work[Random(work)]  # 老婆的职业
+
+        self.Hair = Color[Random(Color)] + Hair[Random(Hair)]  # 发型样式
+
+        self.race = race[Random(race)]  # 种族
+
+        self.name = surname[Random(surname)] + name[Random(name)]  # 姓名
+
+        self.ouBai = ouBaiSize[Random(ouBaiSize)]
+
+        self.Character = Character[Random(Character)]
+
+        self.age = random.randint(16, 24)
+
+        self.height = str(random.randint(150, 170) if self.race != '矮人' else random.randint(120, 140))
+
+        self.weight = str(random.randint(40, 60))
+
+        self.bud = Bud[Random(Bud)]
+
+        self.liking = random.randint(0, 30)
+
+        self.isMerry = False
+
+        self.isTalk = False
+
+        self.scence = None
+
+        self.isMaxTalkNum = False
+    # 最后把生成的对象添加到字典中
+    def addInWifeDict(self):
+        WifeDict[self.name] = self
+
+    def getHusbandId(self) -> str:
+        return self.husband.ID
+
+    def getLoveScence(self) -> object:
+        if self.isMaxTalkNum:
+            self.liking -= 2 if self.liking > -100 else WifeDict.pop(self)
+            if self.liking <= -100: return '你真的很烦，再见!'
+            return '你烦不烦，做你自己的事情去'
+        data = getScence() if self.scence is None else self.scence
+        self.scence = getScence()
+        self.isMaxTalkNum = True
+        threading.Thread(target=self.Delay).start()
+        return data.replace('你',self.HusbandNickName)
+
+    def BanUser(self):
+        self.isMaxTalkNum = False
+
+    def Delay(self):  # 限制访问频率
+        s = sched.scheduler(time.time, time.sleep)
+        s.enter(20, 1, self.BanUser)
+        s.run()
+
+    def couples(self)->str:
+        WifeDict.pop(self.name)
+        return '分手成功'
+
+    def WifeIndex(self) -> str:
+        return f'{self.name}\n' \
+               f'性格：{self.Character}\n' \
+               f'种族：{self.race}\n' \
+               f'职业：{self.work}\n' \
+               f'特点：{self.bud}\n' \
+               f'头发：{self.Hair}\n' \
+               f'瞳色：{self.eyesColor}\n' \
+               f'胸围：{self.ouBai}\n' \
+               f'身高：{self.height}cm\n' \
+               f'体重：{self.weight}kg\n' \
+               f'当前好感度：{self.liking}\n'
+
+
+WifeDict = dict()
---
+++ __init__.py
@@ -0,0 +1,177 @@
+import asyncio
+import json
+import re
+import threading
+import nonebot
+from nonebot import on_command, NLPSession, on_startup, on_natural_language
+from nonebot.command import Command, CommandSession
+from .WifeClass import *
+import difflib
+
+
+def get_equal_rate(str1, str2):
+    return difflib.SequenceMatcher(None, str1, str2).quick_ratio()
+
+
+async def WriteToWifesIndex():
+    with open('index.json', 'w+', encoding='utf-8') as f:
+        src = '['
+        for key in WifeDict:
+            Wifejson = json.dumps(WifeDict[key].getDict(), ensure_ascii=False)
+            src += Wifejson + ',\n'
+        src += ']'
+        src = src.replace(',\n]', ']')
+        f.write(src)
+        f.close()
+
+
+def Read():
+    with open('index.json', 'r+', encoding='utf-8') as f:
+        line = f.readline()
+        while line:
+            line = str(line).replace('[', '').replace(',\n', '').replace(']', '')
+            t = json.loads(line)
+            user_id = husband(t['husband'])
+            temp_wife = WifeObj(user_id)
+            temp_wife.height = t['height']
+            temp_wife.weight = t['weight']
+            temp_wife.name = t['name']
+            temp_wife.ouBai = t['ouBai']
+            temp_wife.liking = t['liking']
+            temp_wife.Character = t['character']
+            temp_wife.age = t['age']
+            temp_wife.isMerry = t['isMerry']
+            temp_wife.work = t['work']
+            temp_wife.race = t['race']
+            temp_wife.bud = t['bud']
+            try:
+                temp_wife.Hair = t['hair']
+                temp_wife.eyesColor = t['eyesColor']
+                temp_wife.WifeNickName = t['WifeNickName']
+                temp_wife.HusbandNickName = t['HusbandNickName']
+            except:
+                pass
+            temp_wife.addInWifeDict()
+            line = f.readline()
+        f.close()
+
+
+@on_startup(func=Read)
+@on_command('getLove', only_to_me=True, aliases=('求分配女朋友'))
+async def getGirlFirend(session: CommandSession):
+    try:
+        UserWife: WifeObj = await searchWife(session, True)
+        await session.send(f'你已经有{UserWife.name}了', at_sender=True)
+        return
+    except:
+        user = session.event['user_id']
+        t = WifeObj(husband(user))
+        await session.send(f'我是{t.name},我愿意与你在一起，今后请多关照')
+        t.addInWifeDict()
+        await WriteToWifesIndex()
+        return
+
+
+bot = nonebot.get_bot()
+
+
+async def searchWife(session: CommandSession, isFirst: bool):
+    id = session.event['user_id']
+    for key in WifeDict:
+        if WifeDict[key].husband.ID == id:
+            return WifeDict[key]
+    else:
+        if not isFirst:
+            await session.send(at_sender=True, message='你还没有女朋友呢，发送“求分配女朋友”来让上帝赐你一位吧，记得要好好珍惜')
+        return -1
+
+
+@on_command('couple', only_to_me=False, aliases='分手')
+async def couple(session: CommandSession):
+    try:
+        UserWife: WifeObj = await searchWife(session, False)
+        src = UserWife.couples()
+        await session.send(at_sender=True, message=src)
+        threading.Thread(target=save).start()
+    except:
+        pass
+
+
+@on_command('help', only_to_me=False, aliases=('!help', '！help'))
+async def help(session: CommandSession):
+    await session.send(message='1.查询女朋友的信息\n'
+                               '2.求分配女朋友\n'
+                               '3.分手\n'
+                               '4.呼叫女朋友的名or近似名\n'
+                               '5.呼叫女朋友的昵称\n'
+                               '6.老婆以后叫我+空格+昵称（请合法使用昵称）\n'
+                               '7.老婆以后我叫你+空格+昵称（请合法使用昵称）\n')
+
+
+@on_command('WifeCallToHusband', only_to_me=False, aliases='老婆以后叫我')
+async def WifeCallToHusband(session: CommandSession):
+    masg = session.current_arg_text.replace('老婆以后叫我', '')
+    try:
+        UserWife: WifeObj = await searchWife(session, False)
+        await session.send(UserWife.setNickName(masg, isWife=False))
+    except:
+        return
+
+
+@on_command('HusbandCallToWife', only_to_me=False, aliases='老婆以后我叫你')
+async def HusbandCallToWife(session: CommandSession):
+    masg = session.current_arg_text.replace('老婆以后我叫你', '')
+    try:
+        UserWife: WifeObj = await searchWife(session, False)
+        await session.send(UserWife.setNickName(masg, isWife=True))
+    except:
+        return
+
+
+@on_command('getWifeIndex', only_to_me=False, aliases='查询女朋友的信息')
+async def getWifeIndex(session: CommandSession):
+    try:
+        userWife: WifeObj = await searchWife(session, False)
+    except:
+        return
+    await bot.send_private_msg(user_id=userWife.husband.ID, message=userWife.WifeIndex())
+    await session.send(at_sender=True, message='已经将我的信息私给你了，要~保~密~哦')
+    await WriteToWifesIndex()
+
+
+@on_natural_language(only_to_me=False)
+async def CallWife(session: NLPSession):
+    t1 = threading.Thread(target=sayLoving, args=(session,))
+    t1.start()
+
+
+def save():
+    loop = asyncio.new_event_loop()
+    a = WriteToWifesIndex()
+    loop.run_until_complete(a)
+
+
+def sayLoving(session: NLPSession):
+    msg = session.msg_text
+    if re.search('CQ:', msg) is not None or not 1 < len(msg) <= 10: return
+    sendUser = session.event['user_id']
+    try:
+        if WifeDict[msg].husband.ID == sendUser:
+            sendLove(WifeDict[msg], session)
+        return
+    except:
+        pass
+    Max: float = 0.4  # 先定相似度
+    for key in WifeDict:
+        t = get_equal_rate(key, msg)
+        if (Max < t or msg == WifeDict[key].WifeNickName) and sendUser == WifeDict[key].getHusbandId():
+            sendLove(WifeDict[key], session)
+            threading.Thread(target=save).start()
+            return
+
+
+def sendLove(A: WifeObj, session: NLPSession):
+    loop = asyncio.new_event_loop()
+    a = session.send(at_sender=True, message=f'{A.name}:{A.getLoveScence()}')
+    A.liking += 2 if A.liking < 200 else 0
+    loop.run_until_complete(a)
